3b79a7d26ed76b94f8c7a708a0bf42d492924952
Rcepenko <rcepenkov@gmail.com>
2024-01-04 00:54:03 +0200

    03.01 Variables

---
+++ Variables and basic data types ✗.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+     //integer (whole number)
+
+    int age = 21;
+    int year = 2024;
+    int days = 7;
+
+    // double (number including decimal) !!!
+
+    double price = 10.99;
+
+    // single character
+
+    char grade = 'A';
+    //char initial = 'B';
+    //char initial = 'BC'; // char can only store a single character !!!
+    char dollarSign = '$';
+
+    // Boolean (true or false)
+
+    bool student = true;
+
+    // String (objects that represents a sequence of text)
+    std::string name = "Roma";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+
+    return 0;
+}
