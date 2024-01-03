8d1bff1ceb9d9e15cbcd87bc144473eda177e9e3
Rcepenko <rcepenkov@gmail.com>
2024-01-04 01:08:51 +0200

    04/01 Const

---
+++ Const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    const double PI = 3.14159; // WHEN CONST THAN BIG LITER
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    const int HEIGHT = 1080;
+    
+    std::cout << circumference << "cm";
+
+
+    return 0;
+}
